4779daffa13244ed9310ba5270247c97d4e4eec0
E-Library-srcas <elibraryforstudents@gmail.com>
2025-02-17 21:30:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ const books = [
         "title": "Introduction to Python",
         "author": "UDAYAN DAS",
         "edition": "1st",
-        "category": "Python",
+        "category": "Programming",
         "url": "https://assets.openstax.org/oscms-prodcms/media/documents/Introduction_to_Python_Programming_-_WEB.pdf",
         "image": "https://m.media-amazon.com/images/I/51JrTiMX+ML._AC_UF1000,1000_QL80_.jpg"
     },
@@ -96,7 +96,7 @@ const books = [
         "title": "Fundamentals of Python",
         "author": "Kenneth A. Lambert",
         "edition": "2nd",
-        "category": "Python",
+        "category": "Programming",
         "url": "https://ggnindia.dronacharya.info/Downloads/Sub-info/RelatedBook/PYTHON-TEXT-BOOK-2.pdf",
         "image": "https://i.ytimg.com/vi/DNCAL1GqOvM/hq720.jpg"
     },
@@ -104,7 +104,7 @@ const books = [
         "title": "Python basics notes",
         "author": "Hans Fangohr",
         "edition": "2nd",
-        "category": "Python",
+        "category": "Programming",
         "url": "https://www.freebookcentre.net/programming-books-download/gotoweb.php?id=11228",
         "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO1MyxpDf0Hx5_KGD0gkzQeFuznTJr98iwwg&s"
     },
@@ -112,7 +112,7 @@ const books = [
         "title": "Introduction to Programming Using Java",
         "author": "David J. Eck",
         "edition": "3rd",
-        "category": "Java",
+        "category": "Programming",
         "url": "https://www.iitk.ac.in/esc101/share/downloads/javanotes5.pdf",
         "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSb4eoXBNHqsGm5N4Bip_u7AXiOujXOBji_of3ckZRu__bnUic9"
     },
@@ -128,7 +128,7 @@ const books = [
         "title": "Data Structures & Algorithms in Python",
         "author": "Mitchael T",
         "edition": "none",
-        "category": "Python",
+        "category": "Programming",
         "url": "https://nibmehub.com/opac-service/pdf/read/Data%20Structures%20and%20Algorithms%20in%20Python.pdf",
         "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1361147120i/13838796.jpg"
     },
@@ -136,7 +136,7 @@ const books = [
         "title": "Learning Python",
         "author": "Mark Lutz",
         "edition": "4th",
-        "category": "Python",
+        "category": "Programming",
         "url": "https://cfm.ehu.es/ricardo/docs/python/Learning_Python.pdf",
         "image": "https://m.media-amazon.com/images/I/91RcdlPx1CL._AC_UF1000,1000_QL80_.jpg"
     },
@@ -144,7 +144,7 @@ const books = [
         "title": "Python Basics",
         "author": "David Amos",
         "edition": "4th",
-        "category": "Python",
+        "category": "Programming",
         "url": "https://static.realpython.com/python-basics-sample-chapters.pdf",
         "image": "https://m.media-amazon.com/images/I/71Q0ZUIyUPL._UF1000,1000_QL80_.jpg"
     },
@@ -152,7 +152,7 @@ const books = [
         "title": "Fluent Python",
         "author": "Luciano Ramalho",
         "edition": "2nd",
-        "category": "Python",
+        "category": "Programming",
         "url": "https://elmoukrie.com/wp-content/uploads/2022/05/luciano-ramalho-fluent-python_-clear-concise-and-effective-programming-oreilly-media-2022.pdf",
         "image": "https://m.media-amazon.com/images/I/81OvszBEdhL._AC_UF1000,1000_QL80_.jpg"
     },
@@ -160,7 +160,7 @@ const books = [
         "title": "Python Programming",
         "author": "Hans-Petter Halvorsen",
         "edition": "none",
-        "category": "Python",
+        "category": "Programming",
         "url": "https://www.halvorsen.blog/documents/programming/python/resources/Python%20Programming.pdf",
         "image": "https://www.halvorsen.blog/documents/programming/python/resources/images/TitlePage1.png"
     },
@@ -168,7 +168,7 @@ const books = [
         "title": "Java in a Nutshell",
         "author": "Benjamin J. Evans & David Flanagan",
         "edition": "6th",
-        "category": "Java",
+        "category": "Programming",
         "url": "https://www.r-5.org/files/books/computers/languages/java/main/Benjamin_Evans_David_Flanagan-Java_in_a_Nutshell_6th_ed-EN.pdf",
         "image": "https://m.media-amazon.com/images/I/91R9esRljJL._AC_UF1000,1000_QL80_.jpg"
     },
@@ -176,7 +176,7 @@ const books = [
         "title": "Learn Java in 21 days",
         "author": "Lauraa Lemay",
         "edition": "none",
-        "category": "Java",
+        "category": "Programming",
         "url": "https://www.cs.cmu.edu/afs/cs.cmu.edu/user/gchen/www/download/java/LearnJava.pdf",
         "image": "https://imgproxy2.pdfroom.com/UdhUjeLDXHy7jmVvrjyiJBrRxV3UW5tbf9XV50zMi0k/rs:auto:200:276:0/g:no/MHEySlFlSkxneEUucG5n.jpg"
     },
@@ -184,7 +184,7 @@ const books = [
         "title": "Java for beginners",
         "author": "Riccardo Flask",
         "edition": "2nd",
-        "category": "Java",
+        "category": "Programming",
         "url": "https://staff.um.edu.mt/__data/assets/pdf_file/0010/57169/jn.pdf",
         "image": "https://2.imimg.com/data2/WE/TT/MY-/6-250x250.jpg"
     },
@@ -192,7 +192,7 @@ const books = [
         "title": "Advanced Java",
         "author": "ANDRIY REDKO",
         "edition": "none",
-        "category": "Java",
+        "category": "Programming",
         "url": "https://enos.itcollege.ee/~jpoial/allalaadimised/reading/Advanced-java.pdf",
         "image": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1515427610i/37855594.jpg"
     },
@@ -200,7 +200,7 @@ const books = [
         "title": "Java Notes for Professionals",
         "author": "Author",
         "edition": "none",
-        "category": "Java",
+        "category": "Programming",
         "url": "https://goalkicker.com/JavaBook/JavaNotesForProfessionals.pdf",
         "image": "https://goalkicker.com/JavaBook/JavaGrow.png"
     },
@@ -208,7 +208,7 @@ const books = [
         "title": "Java for Dummies",
         "author": "Barry Burd",
         "edition": "6th",
-        "category": "Java",
+        "category": "Programming",
         "url": "https://eprints.triatmamulya.ac.id/1702/1/Java%20for%20Dummies%20(6th%20ed.).pdf",
         "image": "https://m.media-amazon.com/images/I/61hDCvdzBvL._AC_UF1000,1000_QL80_.jpg"
     },
@@ -216,7 +216,7 @@ const books = [
         "title": "Java with 101 Examples",
         "author": "Atiwong Suchato",
         "edition": "none",
-        "category": "Java",
+        "category": "Programming",
         "url": "https://www.cp.eng.chula.ac.th/books/wp-content/uploads/sites/5/2018/01/java101.pdf",
         "image": "https://www.cp.eng.chula.ac.th/books/wp-content/uploads/sites/5/2018/01/java101_cover.png"
     },
@@ -224,7 +224,7 @@ const books = [
         "title": "JAVA Programming for Engineers",
         "author": "JULIO SANCHEZ MARIA P. CANTON",
         "edition": "none",
-        "category": "Java",
+        "category": "Programming",
         "url": "https://api.pageplace.de/preview/DT0400.9781420000009_A25118063/preview-9781420000009_A25118063.pdf",
         "image": "https://m.media-amazon.com/images/I/61nQzwnRNJL._AC_UF1000,1000_QL80_.jpg"
     },
@@ -232,7 +232,7 @@ const books = [
         "title": "LEARN JAVATHE EASY WAY",
         "author": "BRYSON PAYNE",
         "edition": "none",
-        "category": "Java",
+        "category": "Programming",
         "url": "https://www.kea.nu/files/textbooks/humblelearn2code/learnjavatheeasyway.pdf",
         "image": "https://m.media-amazon.com/images/I/81EGXCaqmwL._AC_UF1000,1000_QL80_.jpg"
     },
